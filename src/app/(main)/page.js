3462a4091cf2681be73e84a3712a68eed096ca59
Jin Zhang <sh3l6or@icloud.com>
2023-12-27 15:20:51 -0800

    clean up

---
+++ src/app/(main)/page.js
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: 'ProllyYes',
+  description: 'Best online tools for probability and statistics',
+}
+
+export default function Home() {
+  return <>
+      <main></main>
+  </>
+}
