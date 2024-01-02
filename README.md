036d17954a57cdf6e1b4f9c53ad7d45784de340f
Jin Zhang <sh3l6or@icloud.com>
2024-01-02 01:25:08 -0800

    set range and step for params

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Best online tools for probability and statistics.
 - Add pending state when rendering
 - Add Latex to show distributions
 - Browser remember previous parameters when changing distribution
+- Make width of plot responsive
+- Add more distributions and plot types
+- Add statistics section
--- src/app/(routes)/(main)/Parameters.jsx
+++ src/app/(routes)/(main)/Parameters.jsx
@@ -7,11 +7,11 @@ export default function Parameters() {
     <h3>Parameters</h3>
     <div className="h-3"></div>
     <form>
-      <div className="flex flex-wrap gap-3 items-center">
+      <div className="flex flex-col flex-wrap gap-3">
         {Object.keys(params).map(key => (
-          <div key={key} className='flex gap-3 items-center'>
-            <i>{distrParamsNames[distr][key]}</i>
-            <input className="w-16" maxLength="6" placeholder={key}  name={key} value={params[key]} onChange={e => {
+          <div key={key} className='flex gap-3 items-center justify-between'>
+            {distrParamsNames[distr][key]} = {params[key]}
+            <input type='range' min={distrParamsRange[distr][key].min} max={distrParamsRange[distr][key].max} step={distrParamsRange[distr][key].step} className="w-48 dark:text-white" name={key} value={params[key]} onChange={e => {
               setParams({ ...params, [key]: e.target.value })
             }} required />
           </div>
@@ -39,5 +39,58 @@ const distrParamsNames = {
   binom: {
     n: '#Trials',
     p: 'Win Rate'
+  }
+}
+
+const distrParamsRange= {
+  norm: {
+    loc: {
+      min: -5,
+      max: 5,
+      step: 1
+    },
+    scale: {
+      min: 1,
+      max: 10,
+      step: 1
+    }
+  },
+  t: {
+    df: {
+      min: 5,
+      max: 30,
+      step: 1
+    }
+  },
+  f: {
+    dfn: {
+      min: 5,
+      max: 25,
+      step: 1
+    },
+    dfd: {
+      min: 5,
+      max: 25,
+      step: 1
+    }
+  },
+  chi2: {
+    df: {
+      min: 5,
+      max: 25,
+      step: 1
+    }
+  },
+  binom: {
+    n: {
+      min: 10,
+      max: 50,
+      step: 1
+    },
+    p: {
+      min: 0,
+      max: 1,
+      step: 0.1
+    }
   }
 }
