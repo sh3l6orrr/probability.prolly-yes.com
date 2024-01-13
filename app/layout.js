bff312b8f72399ae3c28bfcd08308132a4d3a9f1
Jin Zhang <sh3l6or@icloud.com>
2024-01-12 17:06:46 -0800

    use insights

--- app/layout.js
+++ app/layout.js
@@ -4,11 +4,18 @@ export const viewport = {
     { media: '(prefers-color-scheme: dark)', color: 'black' },
   ],
 }
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="bg-white dark:bg-black">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+        <SpeedInsights />
+
+      </body>
     </html>
   )
 }
