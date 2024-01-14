1ff7cd543dcbf2c2c8dab4ffb1dbe77eb6ea2913
Jin Zhang <sh3l6or@icloud.com>
2024-01-14 14:19:48 -0800

    ad favicon

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,14 @@ export const viewport = {
   ],
 }
 import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from '@vercel/speed-insights/next'
+
+const jsonLd = {
+  "@context": "https://schema.org",
+  "@type": "WebSite",
+  "name": "ProllyYes",
+  "url": "https://www.prolly-yes.com/"
+}
 
 export default function RootLayout({ children }) {
   return (
@@ -14,7 +21,10 @@ export default function RootLayout({ children }) {
         {children}
         <Analytics />
         <SpeedInsights />
-
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
       </body>
     </html>
   )
