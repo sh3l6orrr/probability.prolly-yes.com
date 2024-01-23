10e7a6c67353c219c2ccadac2ca54c7c972c8419
Jin Zhang <sh3l6or@icloud.com>
2024-01-22 19:38:50 -0800

    fix

--- app/(routes)/(main)/Settings.jsx
+++ app/(routes)/(main)/Settings.jsx
@@ -5,6 +5,7 @@ import ParametersSelector from "./ParametersSelector"
 import DistributionSelector from "./DistributionSelector"
 import Panel from "./Panel"
 import { useState, useRef, useEffect } from "react"
+import { InlineMath } from "react-katex"
 
 export default function Settings() {
   const [showSettings, setShowSettings] = useState(null)
@@ -23,9 +24,11 @@ export default function Settings() {
       <Panel />
       <div className="flex border-b px-8 py-4 flex-wrap gap-3 items-center">
         <span>Configure</span>
-        <button className={showSettings === 'distribution' ? 'bg-blue-200 dark:text-black' : ''} onClick={() => setShowSettings(showSettings === 'distribution' ? null : 'distribution')}>Distribution</button>
-        <button className={showSettings === 'parameters' ? 'bg-blue-200 dark:text-black' : ''} onClick={() => setShowSettings(showSettings === 'parameters' ? null : 'parameters')}>Parameters</button>
-        <button className={showSettings === 'utilities' ? 'bg-blue-200 dark:text-black' : ''} onClick={() => setShowSettings(showSettings === 'utilities' ? null : 'utilities')}>Show Utilities</button>
+        <button className={showSettings === 'distribution' ? 'bg-blue-200 dark:text-black' : ''} onClick={() => setShowSettings(showSettings === 'distribution' ? null : 'distribution')}>
+          <InlineMath math={'X'} />
+        </button>
+        <button className={showSettings === 'parameters' ? 'bg-blue-200 dark:text-black' : ''} onClick={() => setShowSettings(showSettings === 'parameters' ? null : 'parameters')}>(...)</button>
+        <button className={showSettings === 'utilities' ? 'bg-blue-200 dark:text-black' : ''} onClick={() => setShowSettings(showSettings === 'utilities' ? null : 'utilities')}>⛭</button>
       </div>
       <div>
         {showSettings === 'distribution' && <DistributionSelector />}
